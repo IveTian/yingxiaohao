ccae66d4a6f89b538055971810ccc07cc73ebf18
田梓衡 <39756011+IveTian@users.noreply.github.com>
2020-04-16 09:18:46 +0800

    📱禁用移动端缩放，优化移动端体验

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0,user-scalable=no">
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
     <title>营销号文章生成器</title>
 
